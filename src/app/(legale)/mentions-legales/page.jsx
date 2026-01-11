d4cadcd6dcb4190f6a4b24c903682404b72724e3
Lenny <lennygomes.hp@gmail.com>
2026-01-11 19:45:48 +0100

    D

--- src/app/(legale)/mentions-legales/page.jsx
+++ src/app/(legale)/mentions-legales/page.jsx
@@ -1,7 +1,7 @@
 export default function page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl text-purple-900">Mentions légales</h1>
+      <h1 className="text-4xl text-purple-900">Mentions légales </h1>
     </main>
   );
 }
